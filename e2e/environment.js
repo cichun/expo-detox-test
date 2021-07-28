388f8a23ab3f4c86883878d7535041d3ab3de2c6
Robert Maciaszczyk <r.maciaszczyk@myaic.com>
2021-07-28 15:13:17 +0200

    fix: expo doesnt open React App
    issue described: https://github.com/cichun/expo-detox-test/issues/1
    this commit shifts initExpo() to right place, now React App successfully opens in Expo Go

--- e2e/environment.js
+++ e2e/environment.js
@@ -9,8 +9,6 @@ class CustomDetoxEnvironment extends DetoxCircusEnvironment {
   constructor(config, context) {
     super(config, context);
 
-    initExpo();
-
     // Can be safely removed, if you are content with the default value (=300000ms)
     this.initTimeout = 300000;
 
@@ -20,6 +18,12 @@ class CustomDetoxEnvironment extends DetoxCircusEnvironment {
       SpecReporter,
       WorkerAssignReporter,
     });
+  }
+
+  async initDetox() {
+    const instance = await this.detox.init(undefined, {launchApp: false});
+    initExpo();
+    return instance;
   }
 }
 
