75fc2a7dc02c2346cad85f9e9bbdae2a63f25739
Robert Maciaszczyk <r.maciaszczyk@myaic.com>
2021-07-28 15:13:58 +0200

    prettier + change default ios sim to iPhone 11

--- e2e/environment.js
+++ e2e/environment.js
@@ -1,30 +1,30 @@
-const { init: initExpo } = require('detox-expo-helpers');
+const {init: initExpo} = require('detox-expo-helpers');
 const {
-  DetoxCircusEnvironment,
-  SpecReporter,
-  WorkerAssignReporter,
+    DetoxCircusEnvironment,
+    SpecReporter,
+    WorkerAssignReporter,
 } = require('detox/runners/jest-circus');
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
-  constructor(config, context) {
-    super(config, context);
+    constructor(config, context) {
+        super(config, context);
 
-    // Can be safely removed, if you are content with the default value (=300000ms)
-    this.initTimeout = 300000;
+        // Can be safely removed, if you are content with the default value (=300000ms)
+        this.initTimeout = 300000;
 
-    // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
-    // This is strictly optional.
-    this.registerListeners({
-      SpecReporter,
-      WorkerAssignReporter,
-    });
-  }
+        // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
+        // This is strictly optional.
+        this.registerListeners({
+            SpecReporter,
+            WorkerAssignReporter,
+        });
+    }
 
-  async initDetox() {
-    const instance = await this.detox.init(undefined, {launchApp: false});
-    initExpo();
-    return instance;
-  }
+    async initDetox() {
+        const instance = await this.detox.init(undefined, {launchApp: false});
+        initExpo();
+        return instance;
+    }
 }
 
 module.exports = CustomDetoxEnvironment;
